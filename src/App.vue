34fe99c0955e9c494519fce3296f6f331ec4c41f
Lucy Scanlon <lucyscanlon@Lucys-MacBook-Pro.local>
2024-07-30 21:35:19 +0100

    started template set up

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <NavBar></NavBar>
+  
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  }
+}
+</script>
+
---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header-nav-bar-container">
+    <div class="header-name-left-container">
+    
+    </div>
+    <div class="header-nav-right-container">
+      
+    </div>
+  </div>
+ 
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+}
+</script>
+
