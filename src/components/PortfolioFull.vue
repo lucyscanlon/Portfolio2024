d42bca4945ab36ab461f1464080ba51e797778fa
Lucy Scanlon <lucyscanlon@Lucys-MacBook-Pro.local>
2024-08-09 18:23:06 +0100

    added language selection panel and the corresponding data in the component

--- src/components/PortfolioFull.vue
+++ src/components/PortfolioFull.vue
@@ -8,14 +8,14 @@
                 <p>Filter projects using the buttons below:</p>
             </div>
             <div class="portfull-language">
-                <span class="green-fill"><button>All</button></span>
-                <button>Vue JS</button>
-                <button>p5.js</button>
-                <button>C++</button>
-                <button>WordPress</button>
-                <button>Freelance</button>
-                <button>Vanilla JavaScript</button>
-                <button>ml5.js</button>
+                <span v-bind:class="(AllStatus === 1) ? 'green-fill' : ''"><button>All</button></span>
+                <button v-bind:class="(VueStatus === 1) ? 'green-fill' : ''">Vue JS</button>
+                <button v-bind:class="(p5Status === 1) ? 'green-fill' : ''">p5.js</button>
+                <button v-bind:class="(CStatus === 1) ? 'green-fill' : ''">C++</button>
+                <button v-bind:class="(WordPressStatus === 1) ? 'green-fill' : ''">WordPress</button>
+                <button v-bind:class="(Freelance === 1) ? 'green-fill' : ''">Freelance</button>
+                <button v-bind:class="(VanJSStatus === 1) ? 'green-fill' : ''">Vanilla JavaScript</button>
+                <button v-bind:class="(ml5Status === 1) ? 'green-fill' : ''">ml5.js</button>
             </div>
         </div>
         <div class="portfull-project-container">
@@ -34,7 +34,14 @@
     name: 'PortfolioFull',
     data() {
         return {
-
+            AllStatus: 1,
+            VueStatus: 0,
+            p5Status: 0,
+            CStatus: 0,
+            WordPressStatus: 0,
+            Freelance: 0,
+            VanJSStatus: 0,
+            ml5Status: 0,
         }
     },
   }
