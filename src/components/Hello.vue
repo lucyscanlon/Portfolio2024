7a0919ada9f656de962a44bc03f37e10c59eb11c
Lucy Scanlon <lucyscanlon@Lucys-MacBook-Pro.local>
2026-05-12 22:04:43 +0100

    commit before 2026 portfolio update

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -8,7 +8,7 @@
                     </div>
                     <div class="polaroid-information-container">
                         <ul>
-                            <li><font-awesome-icon icon="fa-solid fa-map-pin" />London</li>
+                            <li><font-awesome-icon icon="fa-solid fa-map-pin" />Oxfordshire</li>
                             <li><font-awesome-icon icon="fa-solid fa-envelope" />lucyisobelscanlon@gmail.com</li>
                             <a href="https://github.com/lucyscanlon" target="_blank"><li><font-awesome-icon icon="fa-brands fa-github" />github.com/lucyscanlon</li></a>
                         </ul>
@@ -20,10 +20,10 @@
             <div class="introduction-text-right-flex">
                 <div class="introduction-text-padding">
                     <div class="introduction-heading-container">
-                        <h1>Hey!</h1>
+                        <h1>H1: Hi, I’m Lucy. A Web Developer in Oxfordshire.</h1>
                     </div>
                     <div class="introduction-description-container">
-                        <p>Welcome to my portfolio website! I am a recent graduate from Goldsmiths University of London, and am currently looking for an entry level developer role. Please look at my projects to see examples of my work.</p>
+                        <p>I'm a Junior Web Developer based in Oxfordshire with agency and freelance experience. I build responsive, user-focused websites which help businesses communicate clearly, solve real world problems and turn visitors into customers. I’m passionate about continuously developing my skills and building high-quality digital experiences that follow industry best practices.</p>
                     </div>
                     <div class="introduction-grad-container">
                         <p><font-awesome-icon icon="fa-solid fa-user-graduate" /> First Class Honors Degree in Creative Computing</p>
--- src/components/WorkExperience.vue
+++ src/components/WorkExperience.vue
@@ -7,6 +7,83 @@
             <div class="workexperience-list-container">
                 <div class="workexperience-list-padding">
                     <div class="workexperience-list-title-container">
+                        <h3>Junior Web Developer - Website Energizers - Full Time</h3>
+                    </div>
+                    <div class="workexperience-date-container">
+                        <p><font-awesome-icon icon="fa-solid fa-calendar-days" />February 2025 - Present</p>
+                    </div>
+                    <div class="workexperience-list-resp-container">
+                        <h4>Responsibilities</h4>
+                        <ul>
+                            <li>Created bespoke, responsive WordPress websites for a range of businesses as part of the Websites Team.</li>
+                            <li>Worked with the customer to define the website structure and user journey by creating a sitemap.</li>
+                            <li>Developed custom WordPress functionality trailored to individual business needs.</li>
+                            <li>Built responsive websites from Figma designs, accurating translating visual layouts into functional web pages.</li>
+                            <li>Collaborated with the designer to review designs and provide technical guidance to improve build efficiency.</li>
+                            <li>Managed domains, DNS records, hosting environments and ongoing website maintainance.</li>
+                            <li>Investigated and resolved technical issues, providing reliable support to ongoing customers.</li>
+                        </ul>
+                    </div>
+                    <div class="workexperience-skills-container">
+                        <h4>Skills</h4>
+                        <div class="workexperience-typeofskill-container">
+                            <h5>General</h5>
+                            <div class="workexperience-skilllist-container">
+                                <ul>
+                                    <li>WordPress</li>
+                                    <li>PHP</li>
+                                    <li>Image optimisation</li>
+                                    <li>Website structure</li>
+                                    <li>DNS</li>
+                                    <li>Caching</li>
+                                    <li>Plugin conflicts</li>
+                                    <li>Plugin updates</li>
+                                    <li>Responsiveness</li>
+                                    <li>CSS</li>
+                                    <li>jQuery</li>
+                                    <li>WordPress Loop</li>
+                                    <li>Shortcodes</li>
+                                    <li>HTML</li>
+                                    <li>User Experience</li>
+                                    <li>Semantic HTML</li>
+                                    <li>Accessibility</li>
+                                    <li>Search Engine Optimised</li>
+                                    <li>Flex</li>
+                                    <li>Building from a design</li>
+                                    <li>Figma Developer Mode</li>
+                                    <li>Collaboration</li>
+                                    <li>Team Work</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="workexperience-typeofskill-container skilllist-notfirst">
+                            <h5>WordPress Themes</h5>
+                            <div class="workexperience-skilllist-container">
+                                <ul>
+                                    <li>Divi 4</li>
+                                    <li>Divi 5</li>
+                                    <li>Elementor</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="workexperience-typeofskill-container skilllist-notfirst">
+                            <h5>WordPress Plugins</h5>
+                                <div class="workexperience-skilllist-container">
+                                <ul>
+                                    <li>ACF (Advanced Custom Fields)</li>
+                                    <li>WPForms</li>
+                                    <li>WooCommerce</li>
+                                    <li>Lightspeed</li>
+                                    <li>WP Rocket</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="workexperience-list-container">
+                <div class="workexperience-list-padding">
+                    <div class="workexperience-list-title-container">
                         <h3>Digital Assistant - Bloom Hearing Care - Part Time / Remote</h3>
                     </div>
                     <div class="workexperience-date-container">
@@ -15,10 +92,27 @@
                     <div class="workexperience-list-resp-container">
                         <h4>Responsibilities</h4>
                         <ul>
-                            <li>Created web pages to direct users to an online hearing test as part of Bloom's opticians partnership.</li>
+                            <li>Created approximately 20 web pages to direct users to an online hearing test as part of Bloom's opticians partnership.</li>
+                            <li>Created each page to visually match the design of the existing optican's website to ensure a seamless user experience.</li>
                             <li>Created custom pages on WordPress following a brief from Bloom.</li>
                             <li>Worked remotely during the pandemic.</li>
                         </ul>
+                    </div>
+                    <div class="workexperience-skills-container">
+                        <h4>Skills</h4>
+                        <div class="workexperience-typeofskill-container">
+                            <h5>General</h5>
+                            <div class="workexperience-skilllist-container">
+                                <ul>
+                                    <li>WordPress</li>
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                    <li>Responsiveness</li>
+                                    <li>Hosting</li>
+                                    <li>User experience</li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -34,10 +128,27 @@
                         <h4>Responsibilities</h4>
                         <ul>
                             <li>Designed and created websites for three local businesses.</li>
-                            <li>Coded custom WordPress websites.</li>
+                            <li>Coded custom WordPress websites to improve their online presence.</li>
                             <li>Helped clients purchase hosting and domains.</li>
                             <li>Set up the websites on their domain.</li>
                         </ul>
+                    </div>
+                    <div class="workexperience-skills-container">
+                        <h4>Skills</h4>
+                        <div class="workexperience-typeofskill-container">
+                            <h5>General</h5>
+                            <div class="workexperience-skilllist-container">
+                                <ul>
+                                    <li>WordPress</li>
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                    <li>Responsiveness</li>
+                                    <li>Hosting</li>
+                                    <li>User experience</li>
+                                    <li>Website Design</li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
